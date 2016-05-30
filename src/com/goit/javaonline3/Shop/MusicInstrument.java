5ce8b2119ea216083512ddc3ffadee169c37eb3b
vbazylevych <valeriyabazilevich@gmail.com>
2016-05-30 23:57:47 +0300

    5

--- src/com/goit/javaonline3/Shop/MusicInstrument.java
+++ src/com/goit/javaonline3/Shop/MusicInstrument.java
@@ -4,4 +4,13 @@ package com.goit.javaonline3.shop;
  * Created by Ler4enko on 30.05.2016.
  */
 public class MusicInstrument {
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
